aed59593f5d09caa0af07b061f98ec744d3613c7
Ram Varra <ramvarra@hotmail.com>
2022-07-04 12:49:33 -0700

    upd

--- docker_run_jupiterhub-tf.sh
+++ docker_run_jupiterhub-tf.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+
 cur_dir=$(basename $PWD)
-docker run -it \
+
+if which podman > /dev/null; then
+    DOCKER_TOOL=podman
+else
+    DOCKER_TOOL=docker
+fi
+
+echo "Using $DOCKER_TOOL"
+$DOCKER_TOOL run -it --rm \
     -p 11888:8888 \
     -v "$PWD":"/home/jovyan/$cur_dir" \
-    jupyter/tensorflow-notebook
+    docker.io/jupyter/tensorflow-notebook 
--- util.py
+++ util.py
@@ -6,4 +6,5 @@ def show_tf_info(tf):
     print('TF Version:', tf.__version__)
     print('Logical Devices:', tf.config.list_logical_devices())
     print('Physical Devices:', tf.config.list_physical_devices())
+
     
